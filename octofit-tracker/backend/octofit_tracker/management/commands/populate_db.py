4fc3d10954c8c20b8d8b3621a11371ae3a5fa924
brianIgnatiouso <brianIgnatiouso@syncfusion.com>
2026-04-07 11:41:07 +0300

    committing db changes

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from octofit_tracker.models import Team, Activity, Workout, LeaderboardEntry
 from django.utils import timezone
+from pymongo import MongoClient
 
 User = get_user_model()
 
@@ -10,47 +11,41 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            # Clear only data tables
-            LeaderboardEntry.objects.all().delete()
-            Activity.objects.all().delete()
-            Workout.objects.all().delete()
+            # Connect to MongoDB directly to clear collections (Djongo has issues with delete)
+            client = MongoClient('localhost', 27017)
+            db = client['octofit_db']
+            db['octofit_tracker_activity'].delete_many({})
+            db['octofit_tracker_workout'].delete_many({})
+            db['octofit_tracker_leaderboardentry'].delete_many({})
+            db['octofit_tracker_team'].delete_many({})
+            db['octofit_tracker_user'].delete_many({})
+            client.close()
             
-            # Get teams (or create if none exist)
-            teams = Team.objects.all()[:2]
-            if not teams or len(teams) < 2:
-                Team.objects.all().delete()
-                marvel = Team.objects.create(name='Marvel')
-                dc = Team.objects.create(name='DC')
-                teams = [marvel, dc]
-            else:
-                marvel, dc = teams[0], teams[1]
+            # Create teams
+            marvel = Team.objects.create(name='Marvel')
+            dc = Team.objects.create(name='DC')
 
-            # Get users 
-            users = User.objects.exclude(is_superuser=True, is_staff=True)[:4]
-            if users:
-                ironman, spiderman, batman, superman = users[0], users[1], users[2], users[3]
-            else:
-                # Create them if none exist
-                ironman = User.objects.create_user(username='ironman', email='ironman@marvel.com', password='password', first_name='Tony', last_name='Stark')
-                spiderman = User.objects.create_user(username='spiderman', email='spiderman@marvel.com', password='password', first_name='Peter', last_name='Parker')
-                batman = User.objects.create_user(username='batman', email='batman@dc.com', password='password', first_name='Bruce', last_name='Wayne')
-                superman = User.objects.create_user(username='superman', email='superman@dc.com', password='password', first_name='Clark', last_name='Kent')
+            # Create users
+            ironman = User.objects.create_user(username='ironman', email='ironman@marvel.com', password='password', first_name='Tony', last_name='Stark')
+            spiderman = User.objects.create_user(username='spiderman', email='spiderman@marvel.com', password='password', first_name='Peter', last_name='Parker')
+            batman = User.objects.create_user(username='batman', email='batman@dc.com', password='password', first_name='Bruce', last_name='Wayne')
+            superman = User.objects.create_user(username='superman', email='superman@dc.com', password='password', first_name='Clark', last_name='Kent')
 
             # Create activities
-            Activity.objects.create(user=ironman, team_id=str(marvel.pk), type='Running', duration=30, calories=300, date=timezone.now())
-            Activity.objects.create(user=spiderman, team_id=str(marvel.pk), type='Cycling', duration=45, calories=450, date=timezone.now())
-            Activity.objects.create(user=batman, team_id=str(dc.pk), type='Swimming', duration=60, calories=600, date=timezone.now())
-            Activity.objects.create(user=superman, team_id=str(dc.pk), type='Yoga', duration=20, calories=200, date=timezone.now())
+            Activity.objects.create(user=ironman, team=marvel, type='Running', duration=30, calories=300, date=timezone.now())
+            Activity.objects.create(user=spiderman, team=marvel, type='Cycling', duration=45, calories=450, date=timezone.now())
+            Activity.objects.create(user=batman, team=dc, type='Swimming', duration=60, calories=600, date=timezone.now())
+            Activity.objects.create(user=superman, team=dc, type='Yoga', duration=20, calories=200, date=timezone.now())
 
             # Create workouts
             Workout.objects.create(user=ironman, name='Hero HIIT', description='High intensity interval training for heroes.')
             Workout.objects.create(user=batman, name='Power Yoga', description='Yoga for strength and flexibility.')
 
             # Create leaderboard entries
-            LeaderboardEntry.objects.create(user=ironman, team_id=str(marvel.pk), score=75, rank=1, period='weekly')
-            LeaderboardEntry.objects.create(user=spiderman, team_id=str(marvel.pk), score=60, rank=2, period='weekly')
-            LeaderboardEntry.objects.create(user=batman, team_id=str(dc.pk), score=85, rank=1, period='weekly')
-            LeaderboardEntry.objects.create(user=superman, team_id=str(dc.pk), score=70, rank=2, period='weekly')
+            LeaderboardEntry.objects.create(user=ironman, team=marvel, score=75, rank=1, period='weekly')
+            LeaderboardEntry.objects.create(user=spiderman, team=marvel, score=60, rank=2, period='weekly')
+            LeaderboardEntry.objects.create(user=batman, team=dc, score=85, rank=1, period='weekly')
+            LeaderboardEntry.objects.create(user=superman, team=dc, score=70, rank=2, period='weekly')
 
             self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data.'))
         except Exception as e:
--- octofit-tracker/frontend/src/App.test.js
+++ octofit-tracker/frontend/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders OctoFit Tracker app', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const headerElement = screen.getByText(/OctoFit Tracker/i);
+  expect(headerElement).toBeInTheDocument();
 });
